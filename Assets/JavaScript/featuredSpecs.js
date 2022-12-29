073847ad60af8a6f9e11320eec280e1522411eaf
anas599 <anas599@yahoo.com>
2022-12-29 17:10:40 +0300

    Add SCG files for icons

---
+++ Assets/JavaScript/featuredSpecs.js
@@ -0,0 +1,42 @@
+const featuredProducts11 = [
+  {
+    image: './Assets/images/SVG/lipstick-icon.svg',
+    name: 'Foundation',
+    description: 'Ultimate Matte Foundation',
+  },
+  {
+    image: './Assets/images/SVG/makeup-icon.svg',
+    name: 'Primer',
+    description: 'Volume Length Mascara',
+  },
+  {
+    image: './Assets/images/SVG/beauty-icon.svg',
+    name: 'Powder',
+    description: 'Poreless Primer Blurmax Pore-minimize Primer',
+  },
+  {
+    image: './Assets/images/SVG/compact-mirror-icon.svg',
+    name: 'Mascara',
+    description: 'Volume Length Mascara',
+  },
+  {
+    image: './Assets/images/SVG/makeup-brushes-icon.svg',
+    name: 'Eyeliner',
+    description: 'Intense Waterproof Eyeliner',
+  },
+];
+
+const makeUpDiv = document.querySelector('.mainSpecs');
+
+makeUpDiv.innerHTML = featuredProducts11
+  .map(
+    (x) => `
+    <div class='prd1 scroll'>
+        <img class='svgImage' src='${x.image}'>
+        <h4 class='prodName'>${x.name}</h4>
+        <h4 class='prodText'>${x.description}</h4>
+    </div>
+    <br>
+    `,
+  )
+  .join('');
---
+++ Assets/JavaScript/futureProducts.js
@@ -0,0 +1,56 @@
+const featuredProducts = [
+  {
+    image: './Assets/images/foundation1.jpg',
+    imageBoard: './Assets/images/SVG/Chess_Board.svg',
+    name: 'Foundation',
+    description: 'Ultimate Matte Foundation',
+    moreText: 'more text goes here',
+  },
+  {
+    image: './Assets/images/Mascara.jpg',
+    imageBoard: './Assets/images/SVG/Chess_Board.svg',
+    name: 'Primer',
+    description: 'Volume Length Mascara',
+    moreText: 'more text goes here',
+  },
+  {
+    image: './Assets/images/Powder.jpg',
+    imageBoard: './Assets/images/SVG/Chess_Board.svg',
+    name: 'Powder',
+    description: 'Poreless Primer Blurmax Pore-minimize Primer',
+    moreText: 'more text goes here',
+  },
+  {
+    image: './Assets/images/Mascara.jpg',
+    imageBoard: './Assets/images/SVG/Chess_Board.svg',
+    name: 'Mascara',
+    description: 'Volume Length Mascara',
+    moreText: 'more text goes here',
+  },
+  {
+    image: './Assets/images/Eyeliner.jpg',
+    imageBoard: './Assets/images/SVG/Chess_Board.svg',
+    name: 'Eyeliner',
+    description: 'Intense Waterproof Eyeliner',
+    moreText: 'more text goes here',
+  },
+];
+
+const makeUpDiv1 = document.querySelector('.realProd');
+
+makeUpDiv1.innerHTML = featuredProducts
+  .map(
+    (x) => `
+    <div class='realProduct'>
+        <img class='productImage' src='${x.image}'>
+        <img class='productImageBoard' src='${x.imageBoard}'>
+        <div class='productDescription'>
+        <h4 class='prodName2'>${x.name}</h4>
+        <h4 class='prodText2'>${x.description}</h4>
+        <p class='prodMoreText'> ${x.moreText}</p>
+        </div>
+        </div>
+    <br>
+    `,
+  )
+  .join('');
