5aba2d4e069ff875f00fb170cf4e58c5392c00c9
anas599 <anas599@yahoo.com>
2022-12-30 17:10:55 +0300

    Edit Desktop nav bar

--- Assets/JavaScript/navMenuMobile.js
+++ Assets/JavaScript/navMenuMobile.js
@@ -1,15 +1,22 @@
-const heroSection = document.querySelector('.hero');
+const heroSection = document.querySelector('.mainArt');
+const productsListBlur = document.querySelector('.productsList');
 const navIcon = document.querySelector('.navIcon i');
+const dateBlur = document.querySelector('.dateLocation');
 
 // eslint-disable-next-line no-unused-vars
 function navMenuFun() {
   const navMenu = document.querySelector('.navMenu');
   navMenu.classList.toggle('show');
-  heroSection.style.filter = 'blur(0px)';
 
   if (navIcon.classList.contains('fa-bars')) {
     navIcon.classList.replace('fa-bars', 'fa-times');
+    heroSection.style.filter = 'blur(15px)';
+    productsListBlur.style.filter = 'blur(15px)';
+    dateBlur.style.filter = 'blur(15px)';
   } else {
     navIcon.classList.replace('fa-times', 'fa-bars');
+    heroSection.style.filter = 'blur(0px)';
+    productsListBlur.style.filter = 'blur(0px)';
+    dateBlur.style.filter = 'blur(0px)';
   }
 }
